bc50c6411fc960d4df447b546509c25f9a4b98ed
pacoDan <jhonpaco@frba.utn.edu.ar>
2024-04-28 21:36:45 -0300

    :tada: ingreso link desde terminal y no por input

--- descargar.py
+++ descargar.py
@@ -1,33 +1,47 @@
 import yt_dlp
+import sys
 
-link = "ingrese link: "
-url = input(link)
-
-ydl_opts = {}
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([url])
-print("video descargado satsfactoriamente")
-print("Ok nos vemos!!")
-
+# link = "ingrese link: "
+# # url = input(link)
+# #
+# ydl_opts = {}
+# with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+#     ydl.download([url])
+# print("video descargado satsfactoriamente")
+# # print("Ok nos vemos!!")
+#
 
 # import sys
 
-# def procesar_argumentos():
-#     # Verificar si se proporcionó al menos un argumento
-#     if len(sys.argv) < 2:
-#         print("Uso: ./miprograma <directorio>")
-#         print("Si no se proporciona el directorio, se utilizará la carpeta Descargas.")
-#         return None
-#     else:
-#         # El primer argumento es el directorio
-#         return sys.argv[1]
 
-# if __name__ == "__main__":
-#     # Procesar los argumentos
-#     directorio = procesar_argumentos()
+def descargar(link):
+    print("descargando")
+    ydl_opts = {}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([link])
+    print("Descargado!!!")
 
-#     # Si no se proporcionó un directorio, usar la carpeta Descargas
-#     if directorio is None:
-#         directorio = "/home/tu_usuario/Descargas"  # Reemplaza "tu_usuario" con tu nombre de usuario de Linux
 
-#     print("El directorio seleccionado es:", directorio)
+def procesar_argumentos():
+    # Verificar si se proporcionó al menos un argumento
+    if len(sys.argv) < 2:
+        print("Uso: ./descargar link")
+        print("Si no se proporciona el link, se utilizará la carpeta Descargas.")
+        return None
+    else:
+        # El primer argumento es el link
+        return sys.argv[1]
+
+
+if __name__ == "__main__":
+    # Procesar los argumentos
+    link = procesar_argumentos()
+
+    # Si no se proporcionó un link, usar la carpeta Descargas
+    if link is None:
+        # Reemplaza "tu_usuario" con tu nombre de usuario de Linux
+        link = "/home/daniel_wsl2/Descargas"
+    print("El link seleccionado es:", link)
+    print("Comenzando a descargar link ->", link)
+    descargar(link)
+
